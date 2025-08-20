6a58177458d66a50cbf7a1c24e1fb46a775e856f
Olaf Lahl <99406852+aedgarhell@users.noreply.github.com>
2025-08-20 07:23:35 -0700

    Refactor vaults.module.ts to re-export from vaults directory

--- apps/api/src/vaults.module.ts
+++ apps/api/src/vaults.module.ts
@@ -1,10 +1,1 @@
-import { Module } from '@nestjs/common';
-import { VaultsService } from './vaults.service';
-import { VaultsController } from './vaults.controller';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  controllers: [VaultsController],
-  providers: [VaultsService, PrismaService],
-})
-export class VaultsModule {}
+export * from './vaults/vaults.module';
