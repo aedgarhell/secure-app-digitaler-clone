b80c126ad72cea4fdb525e50370ea827a5ecb13c
Olaf Lahl <99406852+aedgarhell@users.noreply.github.com>
2025-08-20 06:55:56 -0700

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { SecretsModule } from './secrets/secrets.module';
+import { VaultsModule } from './vaults/vaults.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, SecretsModule, VaultsModule],
   controllers: [AppController],
   providers: [AppService],
 })
