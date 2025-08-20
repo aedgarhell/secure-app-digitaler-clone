13fe087d0f76586337b9e5dc444919f677abe5bb
Olaf Lahl <99406852+aedgarhell@users.noreply.github.com>
2025-08-20 07:27:28 -0700

    Refactor secrets.module.ts to re-export from secrets directory

--- apps/api/src/secrets.module.ts
+++ apps/api/src/secrets.module.ts
@@ -1,11 +1,1 @@
-import { Module } from '@nestjs/common';
-import { SecretsService } from './secrets.service';
-import { SecretsController } from './secrets.controller';
-import { EncryptionService } from '../encryption/encryption.service';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  controllers: [SecretsController],
-  providers: [SecretsService, EncryptionService, PrismaService],
-})
-export class SecretsModule {}
+export * from './secrets/secrets.module';
