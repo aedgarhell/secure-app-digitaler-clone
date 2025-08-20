073a72eafeadf580a65dddef00075fe3f7c15678
Olaf Lahl <99406852+aedgarhell@users.noreply.github.com>
2025-08-20 10:45:07 -0700

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`API is running on port ${port}`);
 }
-
 bootstrap();
